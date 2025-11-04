044c01e202be70edb9aa8b36d86c5e253c85eabd
Shahryar <shahryar.saleem007@gmail.com>
2025-11-04 17:18:48 -0500

    Added log4j2

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -5,19 +5,24 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import utils.Log;
+
 public class BaseTest {
 
 	protected WebDriver driver;
 	
 	@BeforeMethod
 	public void setUp() {
+		Log.info("Starting WebDriver");
 		driver = new FirefoxDriver();
 		driver.manage().window().maximize();
+		Log.info("navigating to URL...");
 		driver.get("https://admin-demo.nopcommerce.com/login");		
 	}
 	@AfterMethod
 	public void tearDown() {
 		if (driver !=null) {
+			Log.info("Closing Browser");
 			driver.close();
 		}
 	}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -5,14 +5,19 @@ import org.testng.annotations.Test;
 
 import base.BaseTest;
 import pages.LoginPage;
+import utils.Log;
 
 public class LoginTest extends BaseTest {
 
 	@Test
 	public void testValidLogin() {
+		Log.info("Starting Login test ....");
 		LoginPage loginPage = new LoginPage(driver);
+		
+		Log.info("Entering credetials");
 		loginPage.enterUsername("admin@yourstore.com");
 		loginPage.enterPassword("admin");
+		Log.info("Clciking Login Button");
 		loginPage.clickLoginButton();
 		System.out.println(driver.getTitle());
 		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
